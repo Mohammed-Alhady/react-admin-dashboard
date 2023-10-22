7ebbc3d4210b39ba51f08a521250b1343a456c58
Mohammed Alhady <mohammedalhady002@gmail.com>
2023-10-22 20:58:02 +0200

    first commit

---
+++ src/components/Charts/Stacked.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../context/ContextProvider';
+
+
+const Stacked = ({width, heigth}) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={heigth}
+      id='charts'
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{enable: true}}
+      legendSettings={{background: 'white'}}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => 
+          <SeriesDirective 
+            key={index}
+            {...item}
+          />
+        )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  )
+}
+
+export default Stacked
